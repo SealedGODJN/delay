a28e8854a33e9eb697d14fdab58e07f3795b4a57
SealedGodJn <1286039722@qq.com>
2022-03-25 15:05:43 +0800

    2022-3-14 已完成： 1、A653延迟的计算
    待完成：
    1、异步延迟计算逻辑优化
    2、min(PSub,PPub)改为abs(PPub-PSub)？
    3、轨迹法的计算

---
+++ src/main/java/com/nwpu/delay/Message/Parition.java
@@ -0,0 +1,47 @@
+package com.nwpu.delay.Message;
+
+public class Parition {
+    /**
+     * 分区持续时间
+     */
+    public double duration;
+
+    public String id;
+    public String name;
+    /**
+     * 不确定是队列端口还是采样端口
+     */
+    public A664TypePortNode port;
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public A664TypePortNode getPort() {
+        return port;
+    }
+
+    public void setPort(A664TypePortNode port) {
+        this.port = port;
+    }
+}
